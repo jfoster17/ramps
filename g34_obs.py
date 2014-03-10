3fbf7ffca87a94ed90baf15c87a690e2d7f9e8d8
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2014-03-10 19:57:54 -0400

    Add dedicated OnOff and change path to projects

--- g34_obs.py
+++ g34_obs.py
@@ -1,21 +1,21 @@
 # Observing script: MAP of "IRDC43/G34.43".
 
 # Load catalog and configure VEGAS.
-Catalog("/users/jfoster/ramps/ramps_catalog-radec.txt")
+Catalog("/home/astro-util/projects/13B312/ramps/ramps_catalog-radec.txt")
 
 # Point and focus before observing map.
 # See section 10.2 in observing manual.
 # Suggested interval is every 1-2 hours.
 
 #Simple configuration for auto-peak-focus
-execfile("/users/jfoster/ramps/vegas_config_simple.py")
+execfile("/home/astro-util/projects/13B312/ramps/vegas_config_simple.py")
 peak_source = "1845+0953"
 AutoPeakFocus(location=peak_source, flux=1.5, configure=False)
 
 
 ##########  <<<< Do only one of these!!! >>>>> #########
-execfile("/users/jfoster/ramps/vegas_config.py")
-#execfile("/users/jfoster/ramps/vegas_config_simple.py")
+execfile("/home/astro-util/projects/13B312/ramps/vegas_config.py")
+#execfile("/home/astro-util/projects/13B312/ramps/vegas_config_simple.py")
 ##########  <<<< Do only one of these!!! >>>>> #########
 Configure(vegas_config)
 
@@ -38,7 +38,8 @@ execfile("/home/astro-util/projects/TKFPA/kfpaMapInit")
 # KFPA pipeline(?). Ex. in 10.7.
 Slew("G43")
 Balance()
-TargetTrack("G43-peak", None, 30.0, "1")     # check spectra
+OnOff("G43-peak",Offset("Galactic", 0.0, 1.0, cosv=False), 30, "1")
+#TargetTrack("G43-peak", None, 30.0, "1")     # check spectra
 OffTrack("G43-OFF", None, 30.0, "1")    # get off-source obs.
 SetValues("ScanCoordinator",{"scanId":"Map"})
 DecLatMap("G43",                        # center of map
--- snake_obs.py
+++ snake_obs.py
@@ -1,21 +1,21 @@
 # Observing script: MAP of "snake".
 #
 # Load catalog and configure VEGAS.
-Catalog("/users/jfoster/ramps/ramps_catalog.txt")
+Catalog("/home/astro-util/projects/13B312/ramps/ramps_catalog.txt")
 
 # Point and focus before observing map.
 # See section 10.2 in observing manual.
 # Suggested interval is every 1-2 hours.
 
 #Simple configuration for auto-peak-focus
-execfile("/users/jfoster/ramps/vegas_config_simple.py")
+execfile("/home/astro-util/projects/13B312/ramps/vegas_config_simple.py")
 peak_source = "1833-2103"
 AutoPeakFocus(location=peak_source, flux=1.5, configure=False)
 
 
 ##########  <<<< Do only one of these!!! >>>>> #########
-execfile("/users/jfoster/ramps/vegas_config.py")
-#execfile("/users/jfoster/ramps/vegas_config_simple.py")
+execfile("/home/astro-util/projects/13B312/ramps/vegas_config.py")
+#execfile("/home/astro-util/projects/13B312/ramps/vegas_config_simple.py")
 ##########  <<<< Do only one of these!!! >>>>> #########
 Configure(vegas_config)
 
@@ -34,7 +34,8 @@ execfile("/home/astro-util/projects/TKFPA/kfpaMapInit")
 # KFPA pipeline(?). Ex. in 10.7.
 Slew("snake")
 Balance()
-TargetTrack("snake-peak", None, 30.0, "1")     # check spectra
+OnOff("snake-peak",Offset("Galactic", 0.0, 1.0, cosv=False), 30, "1")
+#TargetTrack("snake-peak", None, 30.0, "1")     # check spectra
 OffTrack("snake-off", None, 30.0, "1")    # get off-source obs.
 SetValues("ScanCoordinator",{"scanId":"Map"})
 RALongMap("snake",                        # center of map
